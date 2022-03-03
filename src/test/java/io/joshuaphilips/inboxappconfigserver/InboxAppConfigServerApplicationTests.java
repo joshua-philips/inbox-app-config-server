4610747706f2cf4bf7492c5be3bd452df6f40da9
joshuaphilips <philipsjoshua96@gmail.com>
2022-03-03 22:30:59 +0000

    initial commit

---
+++ src/test/java/io/joshuaphilips/inboxappconfigserver/InboxAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joshuaphilips.inboxappconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
